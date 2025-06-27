1ad44ff20372400e0d11f9dd3e7b0beacc0845bf
Dhruv <dhruvkapure3@gmail.com>
2025-06-27 16:59:10 +0530

    Update main.py

--- NatoAlphabets/main.py
+++ NatoAlphabets/main.py
@@ -1,20 +1,13 @@
 import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
-phonetic_alpha = {}
-for index, row in data.iterrows():
-    phonetic_alpha[row.letter] = row.code
-
-
-# print(phonetic_alpha)
-user_input = input("Enter Your Name: ").upper()
-user_nato = []
-user_split = list(user_input)
-# print(user_split)
-
-for key, value in phonetic_alpha.items():
-    if key in user_split:
-        user_nato.append(value)
-
-
-print(user_nato)
+phonetic_alpha = {row.letter: row.code for index, row in data.iterrows()}
+while True:
+    user_input = input("Enter Your Name: ").upper()
+    user_split = list(user_input)
+    try:
+        user_nato = [phonetic_alpha[letter] for letter in user_split]
+        print(user_nato)
+        break
+    except KeyError:
+        print("Only Alphabets are Allowed")
